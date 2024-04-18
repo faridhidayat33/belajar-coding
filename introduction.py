fb0580bb8245a9e1cb482bbf9c87a822f493b47d
faridhidayat33 <rid.dayat99@gmail.com>
2024-04-18 21:41:29 +0700

    menambahkan komentar pada coding

--- introduction.py
+++ introduction.py
@@ -1,1 +1,7 @@
+print ("nama saya Farid")
+print ("umur saya 25 tahun")
+print ("saya anak pertama dari dua bersaudara")
+print ("saya tinggal di Tangerang")
+print ("saya sedang belajar bahasa pemrograman python")
+print ("5*5=")
 
--- simplemath.py
+++ simplemath.py
@@ -1,3 +1,7 @@
+#cara membuat variabel : <nama variabel> = <isi variabel>
+#input adalah perintah untuk meminta user memasukkan isi dari variabel
+#print adalah perintah untuk mencetak output yang diinginkan
+#output adalah hasil keluaran perintah yang dimasukkan
 a=int(input("masukkan angka #1"))
 b=int(input("masukkan angka #2"))
 penjumlahan=a+b
