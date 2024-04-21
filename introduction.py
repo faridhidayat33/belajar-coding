d1b762dd6210eca09ba7aa0c516e457f903dee60
faridhidayat33 <rid.dayat99@gmail.com>
2024-04-21 23:34:46 +0700

    membuat kalkulator qurban

--- introduction.py
+++ introduction.py
@@ -3,5 +3,4 @@ print ("umur saya 25 tahun")
 print ("saya anak pertama dari dua bersaudara")
 print ("saya tinggal di Tangerang")
 print ("saya sedang belajar bahasa pemrograman python")
-print ("5*5=")
-
+print ("5*5=",5*5)
---
+++ kalkulator-qurban.py
@@ -0,0 +1,4 @@
+a = int (input ("masukkan harga jual sapi : "))
+b = int (input ("masukkan jumlah pengurban sapi : "))
+pembagian = a/b
+print ("jumlah uang yang harus disetor : ", pembagian)
