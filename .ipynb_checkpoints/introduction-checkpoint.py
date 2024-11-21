c57f3e21871ea8dfa87cbd5d1175c3c490da4252
faridhidayat33 <rid.dayat99@gmail.com>
2024-11-21 16:29:24 +0700

    update file coding_class

---
+++ .ipynb_checkpoints/introduction-checkpoint.py
@@ -0,0 +1,6 @@
+print ("nama saya Farid")
+print ("umur saya 25 tahun")
+print ("saya anak pertama dari dua bersaudara")
+print ("saya tinggal di Tangerang")
+print ("saya sedang belajar bahasa pemrograman python")
+print ("5*5=",5*5)
---
+++ .ipynb_checkpoints/simplemath-checkpoint.py
@@ -0,0 +1,24 @@
+#cara membuat variabel : <nama variabel> = <isi variabel>
+#input adalah perintah untuk meminta user memasukkan isi dari variabel
+#print adalah perintah untuk mencetak output yang diinginkan
+#output adalah hasil keluaran perintah yang dimasukkan
+a=int(input("masukkan angka #1"))
+b=int(input("masukkan angka #2"))
+penjumlahan=a+b
+print(penjumlahan)
+
+a=int(input("masukkan angka #1"))
+b=int(input("masukkan angka #2"))
+pengurangan=a-b
+print(pengurangan)
+
+a=int(input("masukkan angka #1"))
+b=int(input("masukkan angka #2"))
+perkalian=a*b
+print(perkalian)
+
+a=int(input("masukkan angka #1"))
+b=int(input("masukkan angka #2"))
+pembagian=a/b
+print(pembagian)
+
